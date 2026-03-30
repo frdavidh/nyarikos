9234d8d91a43721870b388aeb703edaffada74b6
yolooo22 <franklin.david@si.ukdw.ac.id>
2026-03-30 18:59:38 +0900

    add configs

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"github.com/frdavidh/nyarikos/internal/config"
+	"github.com/frdavidh/nyarikos/internal/database"
+	"github.com/frdavidh/nyarikos/internal/logger"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	log := logger.New()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to load config")
+	}
+
+	db, err := database.New(cfg.Database)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to connect to database")
+	}
+
+	mainDB, err := db.DB()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to get database connection")
+	}
+
+	defer mainDB.Close()
+	gin.SetMode(cfg.Server.GinMode)
+
+	log.Info().Msg("starting server")
 }
