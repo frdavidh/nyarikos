01c0d6311cac599873949c1649dd86ec77eb0386
yolooo22 <franklin.david@si.ukdw.ac.id>
2026-04-15 16:31:34 +0900

    add room model

--- backend/internal/models/room.go
+++ backend/internal/models/room.go
@@ -3,18 +3,19 @@ package models
 import (
 	"time"
 
+	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
 )
 
 type Room struct {
-	ID            uint           `json:"id" gorm:"primaryKey"`
-	KostID        uint           `json:"owner_id" gorm:"index;not null"`
-	RoomType      string         `json:"name" gorm:"not null"`
-	PricePerMonth float64        `json:"description" gorm:"index;type:decimal(12,2);not null"`
-	TotalRooms    string         `json:"address" gorm:"not null;default:1"`
-	CreatedAt     time.Time      `json:"created_at"`
-	UpdatedAt     time.Time      `json:"updated_at"`
-	DeletedAt     gorm.DeletedAt `json:"-" gorm:"index" `
+	ID            uint            `json:"id" gorm:"primaryKey"`
+	KostID        uint            `json:"owner_id" gorm:"index;not null"`
+	RoomType      string          `json:"room_type" gorm:"not null"`
+	PricePerMonth decimal.Decimal `json:"price_per_month" gorm:"index;type:decimal(12,2);not null"`
+	TotalRooms    int             `json:"total_rooms" gorm:"not null;default:1"`
+	CreatedAt     time.Time       `json:"created_at"`
+	UpdatedAt     time.Time       `json:"updated_at"`
+	DeletedAt     gorm.DeletedAt  `json:"-" gorm:"index" `
 
 	Kost       Kost       `json:"-" gorm:"foreignKey:KostID" `
 	Facilities []Facility `json:"facilities,omitempty" gorm:"many2many:room_facilities;"`
@@ -27,4 +28,12 @@ type Facility struct {
 	IconURL *string `json:"icon_url"`
 
 	Rooms []Room `json:"-" gorm:"many2many:room_facilities;" `
+}
+
+type RoomFacility struct {
+	RoomID     uint `json:"room_id" gorm:"primaryKey"`
+	FacilityID uint `json:"facility_id" gorm:"primaryKey"`
+
+	Room     Room     `json:"-" gorm:"foreignKey:RoomID"`
+	Facility Facility `json:"-" gorm:"foreignKey:FacilityID"`
 }
